2c7f3d8bb812e5bca303c96d0ece8c2c841100ba
darfux <darfux@163.com>
2014-02-22 14:45:44 +0800

    basely completed
    simply trans the front html to erb

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -3,9 +3,9 @@ class Achievement < ActiveRecord::Base
   validates_presence_of :title, :user_id
 
   searchable do
-    text :title, :as => :project_description
+    text :name, :as => :project_description
   end 
-  
+
   def name
     self.title
   end
--- lib/Searcher.rb
+++ lib/Searcher.rb
@@ -1,11 +1,27 @@
 class SearchResult
+  attr_reader :results, :num
+  def initialize(user, project, achiev, paper)
+    @results = (user+project+achiev+paper).flatten
+    @num = {
+      expert: user.size,
+      project: project.size,
+      achievement: achiev.size,
+      paper: paper.size,
+      total: @results.size
+    }
+  end
+  def each
+    @results.each do |result|
+      yield result
+    end
+  end
 end
 class Searcher
   def self.search(params)
-    user_result = User.search{ fulltext params[:content]}
-    project_result = Project.search { fulltext params[:content] }
-    achievement_result = Achievement.search{ fulltext params[:content]}
-    paper_result = Paper.search{ fulltext params[:content]}
-    (project_result.results<<user_result.results<<achievement_result.results<<paper_result.results).flatten
+    user = User.search{ fulltext params[:content]}
+    project= Project.search { fulltext params[:content] }
+    achievement = Achievement.search{ fulltext params[:content]}
+    paper = Paper.search{ fulltext params[:content]}
+    SearchResult.new(user.results, project.results, achievement.results, paper.results) 
   end
 end
